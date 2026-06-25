ba1f37fc730f9fb7c470400eddb4f2349dfa8736
Naina Agarwal <agarwal.naina1496@gmail.com>
2026-06-25 11:33:57 +0530

    cors

--- twm/main.py
+++ twm/main.py
@@ -1,11 +1,22 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers.health import router as health_api
 from .routers.trip_matcher import router as trip_matcher_api
 
 
 def initialize_app():
     app = FastAPI(title="TravelWithMe Trip Matcher")
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[
+            "https://twm-six.vercel.app",
+        ],
+        allow_origin_regex=r"https://twm-.*\.vercel\.app",
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     app.include_router(health_api)
     app.include_router(trip_matcher_api)
 
